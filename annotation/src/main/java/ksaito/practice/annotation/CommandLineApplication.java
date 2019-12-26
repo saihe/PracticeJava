9bba8a24d111b7b0f545dbe8e8ff80af834cbe90
fbc_saito <saito@fubright.co.jp>
2019-12-26 14:17:07 +0900

    とりあえずスタートした

---
+++ annotation/src/main/java/ksaito/practice/annotation/CommandLineApplication.java
@@ -0,0 +1,10 @@
+package ksaito.practice.annotation;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class CommandLineApplication implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
