aadecde70945bde7810e835845e64e004fbce556
fbc_saito <saito@fubright.co.jp>
2020-03-26 09:46:25 +0900

    モジュール修正

---
+++ migration/src/test/java/ksaito/migration/mssql/MigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ksaito.migration.mssql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
